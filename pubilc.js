5a4173fe2ad92a5e5386c58c817e4eb5a8cbd945
pengxiaoyuan <13658886344@139.com>
2017-07-31 21:37:07 +0800

    新增文件

---
+++ pubilc.js
@@ -0,0 +1,74 @@
+/**
+ * Created by pxy on 2017/7/31.
+ */
+
+/*一、字符串操作*/
+/*去除空格
+ type:
+ 1-所有空格
+ 2-前后空格
+ 3-前空格
+ 4-后空格
+ */
+function trim(str, type) {
+    switch (type) {
+        case 1:
+            return str.replace(/\s+/g, "");
+        case 2:
+            return str.replace(/(^\s*)|(\s*$)/g, "");
+        case 3:
+            return str.replace(/(^\s*)/g, "");
+        case 4:
+            return str.replace(/(\s*$)/g, "");
+        default:
+            return str;
+    }
+}
+/*二、字母大小写切换*/
+/*
+ type
+ 1:首字母大写
+ 2：首页母小写
+ 3：大小写转换
+ 4：全部大写
+ 5：全部小写
+ //changeCase('asdasd',1)
+ //Asdasd
+ * */
+function changeCase(str, type) {
+    function ToggleCase(str) {
+        var itemText = ""
+        str.split("").forEach(
+            function (item) {
+                if (/^([a-z]+)/.test(item)) {
+                    itemText += item.toUpperCase();
+                }
+                else if (/^([A-Z]+)/.test(item)) {
+                    itemText += item.toLowerCase();
+                }
+                else {
+                    itemText += item;
+                }
+            });
+        return itemText;
+    }
+
+    switch (type) {
+        case 1:
+            return str.replace(/^(\w)(\w+)/, function (v, v1, v2) {
+                return v1.toUpperCase() + v2.toLowerCase();
+            });
+        case 2:
+            return str.replace(/^(\w)(\w+)/, function (v, v1, v2) {
+                return v1.toLowerCase() + v2.toUpperCase();
+            });
+        case 3:
+            return ToggleCase(str);
+        case 4:
+            return str.toUpperCase();
+        case 5:
+            return str.toLowerCase();
+        default:
+            return str;
+    }
+}
